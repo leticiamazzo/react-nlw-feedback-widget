5f45723917c12c1b6dda04b0f6317318bb9e1779
Leticia Mazzo <leticiamazzo@gmail.com>
2022-05-05 21:03:56 -0300

    refactor: create popover feedback with headless ui

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,20 +1,12 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
 import { Popover } from '@headlessui/react'
 
 export function Widget() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-    function toggleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen)
-    }
-
     return (
-        <div className="absolute bottom-5 right-5">
-            { isWidgetOpen && <p>Hello Word</p> }
-            
+        <Popover className="absolute bottom-5 right-5">
+            <Popover.Panel>Hello Word</Popover.Panel>
 
-            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6" />
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
@@ -23,7 +15,7 @@ export function Widget() {
                     </span>
                 </span>
 
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
