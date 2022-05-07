c4763b48fc95ea714ef9aad6fea26fbe3ef7e539
Leticia Mazzo <leticiamazzo@gmail.com>
2022-05-06 23:22:05 -0300

    feat: create return to first step button

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -37,6 +37,10 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null)
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             {!feedbackType ? (
@@ -44,7 +48,10 @@ export function WidgetForm() {
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
                 // Manda para componente FeedbackContentStep qual foi o tipo de feedback que usuário escolheu
-                <FeedbackContentStep feedbackType={feedbackType} />
+                <FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
 
             <footer className="text-xs text-neutral-400">
